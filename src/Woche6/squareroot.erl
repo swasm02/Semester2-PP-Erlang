2233f3c5fefed45327c8a6a75d81c4cb6400ab0d
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-05-12 10:25:11 +0200

    recursion ends now when a certain level is reached

--- src/Woche6/squareroot.erl
+++ src/Woche6/squareroot.erl
@@ -10,12 +10,20 @@
 -author("swasm").
 
 %% API
--export([wurzel/1]).
+-export([wurzel/1, wurzel2/2]).
 
-wurzel(X) -> wurzel(X, 1, 0.001).
-
+wurzel(X) -> wurzel(X, 1, 0.0001).
 wurzel(X,Y,Epsilon) ->
   case abs(Y*Y - X) < Epsilon of
     true -> Y;
     false -> wurzel(X, 1/2 * (Y + X/Y), Epsilon)
+  end.
+
+wurzel2(X, U) -> wurzel2(X, 1, U).
+wurzel2(X,Y, U) ->
+  Z = 1/2 * (Y + X/Y),
+  %io:fwrite("Y: ~p  Z: ~p ", [Y, Z]),
+  case abs(Y-Z) < U of
+    true -> Y;
+    false -> wurzel2(X, Z , U)
   end.
