5233ddbd7c5cb2333efb2a17f4c32d75479346e6
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-05-12 09:20:27 +0200

    wrote a faster endrecursive version

--- src/Woche6/mulList.erl
+++ src/Woche6/mulList.erl
@@ -10,7 +10,13 @@
 -author("swasm").
 
 %% API
--export([multiply/1]).
+-export([multiply/1, multendrec/1]).
 
 multiply([]) -> 1;
 multiply([L | Lremains]) -> L * multiply(Lremains).
+
+% endrekursiv
+multendrec(L) -> multendrec(L, 1).
+
+multendrec([], End) -> End;
+multendrec([L | Lremains], End) -> multendrec(Lremains, L * End).
