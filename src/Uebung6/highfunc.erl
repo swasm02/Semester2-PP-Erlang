c27b098d215d0f3a9bd3314be496daba085f257c
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-05-22 14:10:39 +0200

    Übungsstunde

--- src/Uebung6/highfunc.erl
+++ src/Uebung6/highfunc.erl
@@ -16,5 +16,11 @@ square(N) -> N*N.
 
 map(_, []) -> [];
 map(Func, [H|T]) -> [Func(H)|map(Func,T)].
-
 % Aufruf erfolgt mit highfunc:map(fun highfunc:square/1, [1,2,3,4]).
+
+% Übungsstunde:
+q(L) -> lists:map(fun square/1, L).
+
+%alternativ mit anonymer Funktion:
+q2(L) -> lists:map(fun(E) -> E*E end, L).
+% "fun(E) -> E*E end" ist Closure
