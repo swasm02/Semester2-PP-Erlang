ee132c001751fb940eb407c13df684f43719a9b2
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-06-05 13:30:13 +0200

    Übungsstunde

--- src/Uebung6/filter.erl
+++ src/Uebung6/filter.erl
@@ -5,7 +5,7 @@
 %%%
 %%% @end
 %%% Created : 18. Mai 2023 13:37
-%%%-------------------------------------------------------------------
+%%%------------------------------------------------------------------
 -module(filter).
 -author("swasm").
 
@@ -13,8 +13,20 @@
 -export([eqklassen/2, modThree/1]).
 
 modThree(N) -> N rem 3.
+id(X) -> X.
 
 eqklassen(Func, List) -> filter([eq_helper(Func, List, X) || X <- List], []).
+%% falsch, lists:filter verwenden!!!
+
+% Chris Lösung:
+eqclass (F, Ls) -> [[X || X <- Ls, F(X) =:= Key] || Key <- lists:usort([F(X) || X <- Ls])].
+
+% Übunststunde Lösung:
+eqklassenuebung(_, []) -> [];
+eqklassenuebung(F, [H|T]) ->
+  E = F(H),
+  L = [X || X <- [H|T], F(X) == E],
+  [L | eqklassenuebung(F, T -- L)].
 
 eq_helper(Func, List, X) -> [Y || Y <- List, Func(X) =:= Func(Y)].
 
