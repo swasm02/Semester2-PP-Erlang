744f85ccf102dc1bebff1b7d5fc50f88bc86869d
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-06-12 14:16:09 +0200

    Übungsstunde

--- src/uebung8/infList.erl
+++ src/uebung8/infList.erl
@@ -9,9 +9,10 @@
 -module(infList).
 -author("swasm").
 
-%% API
--export([prim/1]).
+%%% API ---------------------------------------------------------------
+-export([prim/1, primes/1]).
 
+% Aufgabenstellung falsch verstanden :(
 prim(N) ->
     lists:reverse(eratosthenes(toList(N, []))).
 
@@ -28,3 +29,19 @@ eratosthenes([H | T], Acc) ->
 
 aussortieren(List, N) ->
   lists:filter(fun(X) -> X rem N =/= 0 end, List).
+
+%%%-------------------------------------------------------------------
+%% Mikas Lösung
+primes(K) ->
+  sieb(from(2), K).
+
+from(A) -> [A | fun() -> from(A + 1) end].
+
+sieb([H | T], K) when K > 0 ->
+  [H | sieb(aussortieren2(T(), H), K - 1)];
+sieb(_, 0) -> [].
+
+aussortieren2([H | T], N) when (H rem N) =:= 0 ->
+  aussortieren2(T(), N);
+aussortieren2([H | T], N) ->
+  [H | fun() -> aussortieren2(T(), N) end].
