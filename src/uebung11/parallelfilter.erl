83621b4e4ff4238cc373662f00313e8bf06cf486
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-06-30 14:31:37 +0200

    a1 uebung 11

---
+++ src/uebung11/parallelfilter.erl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author swasm
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30. Jun 2023 14:26
+%%%-------------------------------------------------------------------
+-module(parallelfilter).
+-author("swasm").
+
+%% API
+-export([filter_parallel/3, filter_spawn/2]).
+
+% Version 1: Filtern mit spawn für jedes Element
+filter_spawn(_, []) -> [];
+filter_spawn(F, [H|T]) ->
+  Pid = spawn(fun() -> filter_spawn(F, T) end),
+  case F(H) of
+    true -> [H|Pid];
+    false -> Pid
+  end.
+
+% Version 2: Parallelisierung durch Aufteilung der Eingabeliste
+filter_parallel(_, [], _) -> [];
+filter_parallel(F, L, K) ->
+  Sublists = lists:split(K, L),
+  Pids = [spawn(fun() -> filter_parallel(F, Sublist, K) end) || Sublist <- Sublists],
+  Results = [Result || Pid <- Pids, Result <- Pid],
+  lists:flatten(Results).
