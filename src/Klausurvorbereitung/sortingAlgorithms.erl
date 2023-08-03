be4c98c1fddf04d2a4184a493ce3f3c3ea8c95d0
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-08-03 18:38:35 +0200

    Klausurvorbereitung

---
+++ src/Klausurvorbereitung/sortingAlgorithms.erl
@@ -0,0 +1,53 @@
+%%%-------------------------------------------------------------------
+%%% @author swasm
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. Aug 2023 18:37
+%%%-------------------------------------------------------------------
+-module(sortingAlgorithms).
+-author("swasm").
+
+%% API
+-export([]).
+
+ms([],K)->[];
+ms(X,K) when length(X) == 1 ->X;
+ms(L,K)->
+  Parent = self(),
+  {L1 , L2} = lists:split(length(L) div 2, L),
+  case (K > 0) of
+    true -> spawn(fun()-> Parent ! ms(L1, K-1) end),
+      L22 = ms(L2,K-1),
+      merge(receive T2 -> T2 end, L22);
+    false -> L12 = ms(L1, K),
+      L22 = ms(L2, K),
+      merge(L12, L22)
+  end.
+
+merge([H1| T1], [H2|T2])->
+  case (H1 < H2) of
+    true -> [H1 | merge(T1, [H2|T2])];
+    false -> [H2 | merge([H1|T1], T2)]
+  end;
+merge([],L)-> L;
+merge(L,[])-> L.
+
+
+
+test(K) ->
+  Seq = lists:seq(1,1000),
+  Data = lists:map(fun() -> random:uniform(999998)+2 end, Seq),
+  ms(Data, K).
+
+benchmark(K) ->
+  Times = [timer:tc(mergesort, test, [K]) || K <- lists:seq(1, 100)],
+  lists:sum([T || {T, _} <- Times]) / length(Times).
+sort(L) -> lists:foldl(fun insertion/2,[],L).   % schieben in neue Liste
+
+insertion(E, []) -> [E];
+insertion(E,[X|Y]) -> if
+                        E < X -> [E,X |Y];
+                        true -> lists:append([X], insertion(E, Y))
+                      end.
